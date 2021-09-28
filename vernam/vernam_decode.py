958fed1810852dcc2e933a7c3394e7fc7997fa3c
Euclidius <perfodio@yandex.ru>
2021-09-28 08:53:19 +0700

    Started guessing key

--- vernam/vernam_decode.py
+++ vernam/vernam_decode.py
@@ -2,18 +2,18 @@ with open('bin_output.txt') as f:
     encrypted = f.read()
 
 encrypted = list(encrypted)
-encrypted = [ord(i) for i in encrypted]
+encrypted_ord = [ord(i) for i in encrypted]
 
-for key in range(2, 21):
+for key in range(1, 21):
     shifted_text = []
     letters = {}
     
-    for j in range(len(encrypted)-key):
-        shifted_text.append(encrypted[j + key])
+    for j in range(len(encrypted_ord)-key):
+        shifted_text.append(encrypted_ord[j + key])
     
     shift_xor_encrypt = []
     for i in range(len(shifted_text)):
-        shift_xor_encrypt.append(int(shifted_text[i]) ^ int(encrypted[i]))
+        shift_xor_encrypt.append(int(shifted_text[i]) ^ int(encrypted_ord[i]))
     
     unique_letters = []
     for i in range(len(shift_xor_encrypt)):
@@ -29,5 +29,12 @@ for key in range(2, 21):
     maybe_values = sum / len(shifted_text)
     print(maybe_values)
     
-length = int(input("Print the len of the key: "))
-print(length)
+lenght = int(input("Print the len of the key: "))
+
+
+one_letter_enc = []
+for i in range(0, len(encrypted_ord)//lenght, lenght):
+    
+    one_letter_enc.append(encrypted[i])
+
+print(letters)
